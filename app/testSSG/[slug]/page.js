ce9acf37698748a301397676089084d84e555cdc
leichao <>
2024-05-31 20:53:17 +0800

    feat: 客户端组件报错的各种解决办法

--- app/testSSG/[slug]/page.js
+++ app/testSSG/[slug]/page.js
@@ -16,12 +16,12 @@ export default async function BlogPost({ params }) {
 
   return (
     <div>
-      {/* ISR和SSG一起使用 */}
-      <button onClick={async () => {
+      {/* ISR和SSG一起使用 下面的button必须放在客户端组件中*/}
+      {/* <button onClick={async () => {
           const res = await fetch('https://jsonplaceholder.typicode.com/posts')
           const posts = await res.json()
           setData(posts.slice(10, 20))
-      }}>换一批</button>
+      }}>换一批</button> */}
       <h2>标题：{post.title}</h2>
       <h2>id: {post.id}</h2>
       <p>内容{post.body}</p>
